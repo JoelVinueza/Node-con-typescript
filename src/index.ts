6d4c740f45642e31e74fb688ecf29188d48ec79a
Joel Vinueza <jvinuezag1@est.ups.edu.ec>
2024-06-21 22:06:08 -0500

    Avance 00

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,14 @@ const server = http.createServer(app);
 const io = new SocketIOServer(server);
 
 io.on('connection', (socket) =>{
-    console.log('A user connected');
+    console.log('A user connected:', socket.id);
     
     socket.on('disconnected', () =>{
-        console.log('A user disconnected');
+        console.log('A user disconnected:', socket.id);
     });
+
+
 });
 
 server.listen(PORT);
 console.log('Server on port:', PORT);
-
-
